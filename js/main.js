a9e8b90a2771bb08d7f0c3904d06da0f385c8e52
ryanjeffrey <ryanjeffreysmith@gmail.com>
2020-12-23 22:08:16 -0600

    Added prompt on button click

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
+$(document).ready(function(){
+
 var eightBall = {};
 eightBall.listOfAnswers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes – definitely.", "You may rely on it.", "As I see it - yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy - try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
  
 eightBall.askQuestion = function (question) {
+	prompt("I can tell your future with little to some level of accuracy. Try me out by asking a Yes or No question.");
+
 	randomNumber = Math.random();
  
 	randomNumberArray = randomNumber * this.listOfAnswers.length;
@@ -12,4 +16,7 @@ eightBall.askQuestion = function (question) {
   		console.log("Q: " + question + " A: " + randomAnswer);
 }
 
-eightBall.askQuestion ("Am I winning the battle with JavaScript?");
+
+$("#questionButton").click(eightBall.askQuestion);
+
+});
